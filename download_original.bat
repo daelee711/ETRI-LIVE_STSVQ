cd67128e4814beafbbbc1d8a3e1ee91812e3b81a
daelee711 <42107634+daelee711@users.noreply.github.com>
2023-05-21 16:55:11 -0700

    Download scripts

---
+++ download_original.bat
@@ -0,0 +1,17 @@
+:: Download script for original videos
+
+call curl -L https://utexas.box.com/shared/static/g95bb1q7xjsub6wbdcgeftkkhrq1r6zz.yuv --output Yacht_2160p_120hz.yuv
+call curl -L https://utexas.box.com/shared/static/z8fyfmklkf901as5kg5nms6k97j2b3j6.yuv --output Beauty_2160p_120hz.yuv
+call curl -L https://utexas.box.com/shared/static/3bn1l1ra3kw1veeisyon56kpr5svdtx6.yuv --output HoneyBee_2160p_120hz.yuv
+call curl -L https://utexas.box.com/shared/static/d8fsrfruchyuixrpzvr2s92dt10teaqu.yuv --output Jockey_2160p_120hz.yuv
+call curl -L https://utexas.box.com/shared/static/0sc73v0v1t25rkpad0rt14w60qrspioo.yuv --output Ready_2160p_120hz.yuv
+call curl -L https://utexas.box.com/shared/static/g50s8wxh62z0cobo0qdsqzvpz2iklan2.yuv --output Football_2160p_60hz.yuv
+call curl -L https://utexas.box.com/shared/static/6z964bqeciotyoof1m2wkj22jaq6juiq.yuv --output Monkeys_2160p_60hz.yuv
+call curl -L https://utexas.box.com/shared/static/cdusws7rmg65fq67rnw3pcnadhqub4h9.yuv --output Dancers_2160p_60hz.yuv
+call curl -L https://utexas.box.com/shared/static/bstn6o2lww94wdejjdz4nzpkxo9lzl63.yuv --output Dinner_2160p_60hz.yuv
+call curl -L https://utexas.box.com/shared/static/ayr3ou2qffne9w19wfexmeofkhskq9xd.yuv --output Toddler_2160p_60hz.yuv
+call curl -L https://utexas.box.com/shared/static/20brt6cdx54gwzboeh180dgoqstxkdz2.yuv --output Narrator_2160p_60hz.yuv
+call curl -L https://utexas.box.com/shared/static/c5pvai6hlhx2632p3n1nmfs36vioxiki.yuv --output Tunnel_2160p_60hz.yuv
+call curl -L https://utexas.box.com/shared/static/1e7s9bv200kw7skh38o0cmee26u9r1o2.yuv --output Ritual_2160p_60hz.yuv
+call curl -L https://utexas.box.com/shared/static/aml1gf21xsp10f52oqed2c3f6s1lqqex.yuv --output Discuss_2160p_60hz.yuv
+call curl -L https://utexas.box.com/shared/static/00vswwdcni9xhhqv3baju347pz0vhxee.yuv --output SeaRock_2160p_60hz.yuv
---
+++ download_original.sh
@@ -0,0 +1,18 @@
+# !/bin/bash
+# Download script for original videos
+
+wget -L https://utexas.box.com/shared/static/g95bb1q7xjsub6wbdcgeftkkhrq1r6zz.yuv -O Yacht_2160p_120hz.yuv -q --show-progress
+wget -L https://utexas.box.com/shared/static/z8fyfmklkf901as5kg5nms6k97j2b3j6.yuv -O Beauty_2160p_120hz.yuv -q --show-progress
+wget -L https://utexas.box.com/shared/static/3bn1l1ra3kw1veeisyon56kpr5svdtx6.yuv -O HoneyBee_2160p_120hz.yuv -q --show-progress
+wget -L https://utexas.box.com/shared/static/d8fsrfruchyuixrpzvr2s92dt10teaqu.yuv -O Jockey_2160p_120hz.yuv -q --show-progress
+wget -L https://utexas.box.com/shared/static/0sc73v0v1t25rkpad0rt14w60qrspioo.yuv -O Ready_2160p_120hz.yuv -q --show-progress
+wget -L https://utexas.box.com/shared/static/g50s8wxh62z0cobo0qdsqzvpz2iklan2.yuv -O Football_2160p_60hz.yuv -q --show-progress
+wget -L https://utexas.box.com/shared/static/6z964bqeciotyoof1m2wkj22jaq6juiq.yuv -O Monkeys_2160p_60hz.yuv -q --show-progress
+wget -L https://utexas.box.com/shared/static/cdusws7rmg65fq67rnw3pcnadhqub4h9.yuv -O Dancers_2160p_60hz.yuv -q --show-progress
+wget -L https://utexas.box.com/shared/static/bstn6o2lww94wdejjdz4nzpkxo9lzl63.yuv -O Dinner_2160p_60hz.yuv -q --show-progress
+wget -L https://utexas.box.com/shared/static/ayr3ou2qffne9w19wfexmeofkhskq9xd.yuv -O Toddler_2160p_60hz.yuv -q --show-progress
+wget -L https://utexas.box.com/shared/static/20brt6cdx54gwzboeh180dgoqstxkdz2.yuv -O Narrator_2160p_60hz.yuv -q --show-progress
+wget -L https://utexas.box.com/shared/static/c5pvai6hlhx2632p3n1nmfs36vioxiki.yuv -O Tunnel_2160p_60hz.yuv -q --show-progress
+wget -L https://utexas.box.com/shared/static/1e7s9bv200kw7skh38o0cmee26u9r1o2.yuv -O Ritual_2160p_60hz.yuv -q --show-progress
+wget -L https://utexas.box.com/shared/static/aml1gf21xsp10f52oqed2c3f6s1lqqex.yuv -O Discuss_2160p_60hz.yuv -q --show-progress
+wget -L https://utexas.box.com/shared/static/00vswwdcni9xhhqv3baju347pz0vhxee.yuv -O SeaRock_2160p_60hz.yuv -q --show-progress
